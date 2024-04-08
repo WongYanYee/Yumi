6ad885af034d4613d5c77ad8158faffc7a8f5711
WongYanYee <166373454+WongYanYee@users.noreply.github.com>
2024-04-08 17:17:54 +0800

    Update Q1.1code.py

--- Q1.1code.py
+++ Q1.1code.py
@@ -1,8 +1,10 @@
+#import modules for web scraping. 
 import requests
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
 
+#search for a specfic pattern within a movie url.
 def extract_movie_id(url):
     match = re.search(r'gr(\d+)/', url)
     if match:
@@ -10,14 +12,14 @@ def extract_movie_id(url):
         return "gr" + group_id
     else:
         return None
-
+#scrap data from website BoxOfficeMojo.
 if __name__ == "__main__":
     
     url = f"https://www.boxofficemojo.com/year/world/2023/"
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        tables = soup.find_all('table', class_='mojo-body-table')
+        tables = soup.find_all('table', class_='mojo-body-table') #find all tables that have the class attribute to 'mojo body table'
         if tables:
             with open("TopMoviesBoxOffice.txt", "w", encoding="utf-8") as file:
                 for table in tables:
@@ -25,11 +27,12 @@ if __name__ == "__main__":
                     for row in rows:
                         columns = row.find_all('td')
                         if columns:
-                            rank = columns[0].text.strip()
+                            rank = columns[0].text.strip() 
                             movie_name = columns[1].text.strip().replace(",", "")
                             movie_url = columns[1].find('a')['href']
                             movie_id = extract_movie_id(movie_url)
                             worldwide_boxoffice = columns[2].text.strip().replace(",", "").replace("$", "")
                             domestic_boxoffice = columns[3].text.strip().replace(",", "").replace("$", "")
-                            file.write(f"{rank},{movie_name},{movie_id},{worldwide_boxoffice},{domestic_boxoffice}\n")
+                           
+                            file.write(f"{rank},{movie_name},{movie_id},{worldwide_boxoffice},{domestic_boxoffice}\n") #a formatted string to the file, extract value of 5 items
         print("TopMoviesBoxOffice.txt")
